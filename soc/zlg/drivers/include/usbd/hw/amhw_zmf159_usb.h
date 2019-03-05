ff22b04a8de7fa82413e373bd3d020243e4014c9
yangyue <yangyue@zlg.cn>
2019-03-05 09:00:26 +0800

    测试temp工程通过

--- soc/zlg/drivers/include/usbd/hw/amhw_zmf159_usb.h
+++ soc/zlg/drivers/include/usbd/hw/amhw_zmf159_usb.h
@@ -41,59 +41,36 @@ extern "C" {
  */
 typedef struct amhe_zmf159_usb{
     __I  uint32_t reserve0[3];   /**< \brief ±£Áô  */
-
-    __I  uint16_t add_info;      /**< \brief ADD_INFO Æ«ÒÆ£º0x0c */
-    __I  uint16_t reserve1;      /**< \brief ±£Áô  */
-
+    __I  uint32_t add_info;      /**< \brief ADD_INFO Æ«ÒÆ£º0x0c */
     __I  uint32_t reserve2[28];  /**< \brief ±£Áô  */
-
-    __IO uint16_t int_stat;      /**< \brief ÖĞ¶Ï×´Ì¬¼Ä´æÆ÷ Æ«ÒÆ£º0x80 */
-    __I  uint16_t reserve3;      /**< \brief ±£Áô  */
-
-    __IO uint16_t int_enb;       /**< \brief ÖĞ¶ÏÊ¹ÄÜ¼Ä´æÆ÷ Æ«ÒÆ£º0x84 */
-    __I  uint16_t reserve4;      /**< \brief ±£Áô  */
-
-    __IO uint16_t err_stat;      /**< \brief ´íÎóÖĞ¶Ï¼Ä´æÆ÷ Æ«ÒÆ£º0x88 */
-    __I  uint16_t reserve5;      /**< \brief ±£Áô  */
-
-    __IO uint16_t stat;          /**< \brief ×´Ì¬¼Ä´æÆ÷ Æ«ÒÆ£º0x90 */
-    __I  uint16_t reserve6;      /**< \brief ±£Áô  */
-
-    __IO uint16_t ctl;           /**< \brief ¿ØÖÆ¼Ä´æÆ÷ Æ«ÒÆ£º0x94 */
-    __I  uint16_t reserve7;      /**< \brief ±£Áô  */
-
-    __IO uint16_t addr;          /**< \brief µØÖ·¼Ä´æÆ÷ Æ«ÒÆ£º0x98 */
-    __I  uint16_t reserve8;      /**< \brief ±£Áô  */
-
-    __IO uint16_t bdt_page01;    /**< \brief bufferÃèÊöÒ³¼Ä´æÆ÷1 Æ«ÒÆ£º0x9c */
-    __I  uint16_t reserve9;      /**< \brief ±£Áô  */
-
-    __IO uint16_t frm_numl;      /**< \brief ¿ò¼ÜÊı×Ö¼Ä´æÆ÷ Æ«ÒÆ£º0xa0 */
-    __I  uint16_t reserve10;     /**< \brief ±£Áô  */
-
-    __IO uint16_t frm_numh;      /**< \brief ¿ò¼ÜÊı×Ö¼Ä´æÆ÷ Æ«ÒÆ£º0xa4 */
-    __I  uint16_t reserve11;     /**< \brief ±£Áô  */
-
-    __IO uint16_t token;         /**< \brief Ö¸Áî¼Ä´æÆ÷ Æ«ÒÆ£º0xa8 */
-    __I  uint16_t reserve12;     /**< \brief ±£Áô  */
-
-    __IO uint16_t sof_thld;      /**< \brief SOFãĞ´æÆ÷ Æ«ÒÆ£º0xac */
-    __I  uint16_t reserve13;     /**< \brief ±£Áô  */
-
-    __IO uint16_t bdt_page02;    /**< \brief bufferÃèÊöÒ³¼Ä´æÆ÷2 Æ«ÒÆ£º0xb0 */
-    __I  uint16_t reserve14;     /**< \brief ±£Áô  */
-
-    __IO uint16_t bdt_page03;    /**< \brief bufferÃèÊöÒ³¼Ä´æÆ÷3 Æ«ÒÆ£º0xb4 */
-    __I  uint16_t reserve15;     /**< \brief ±£Áô  */
-
-    __I  uint32_t reserve16[2];  /**< \brief ±£Áô  */
-
-    struct {
-        __IO uint16_t endpt;     /**< \brief ¶Ëµã¿ØÖÆ¼Ä´æÆ÷ Æ«ÒÆ£º0xc0 ~ 0xfc */
-        __I  uint16_t reserve17; /**< \brief ±£Áô  */
-    } endpt_n[16];
+    __IO uint32_t int_stat;      /**< \brief ÖĞ¶Ï×´Ì¬¼Ä´æÆ÷ Æ«ÒÆ£º0x80 */
+    __IO uint32_t int_enb;       /**< \brief ÖĞ¶ÏÊ¹ÄÜ¼Ä´æÆ÷ Æ«ÒÆ£º0x84 */
+    __IO uint32_t err_stat;      /**< \brief ´íÎóÖĞ¶Ï¼Ä´æÆ÷ Æ«ÒÆ£º0x88 */
+    __IO uint32_t err_enb;       /**< \brief ´íÎóÖĞ¶Ï¼Ä´æÆ÷ Æ«ÒÆ£º0x8C */
+    __IO uint32_t stat;          /**< \brief ×´Ì¬¼Ä´æÆ÷ Æ«ÒÆ£º0x90 */
+    __IO uint32_t ctl;           /**< \brief ¿ØÖÆ¼Ä´æÆ÷ Æ«ÒÆ£º0x94 */
+    __IO uint32_t addr;          /**< \brief µØÖ·¼Ä´æÆ÷ Æ«ÒÆ£º0x98 */
+    __IO uint32_t bdt_page01;    /**< \brief bufferÃèÊöÒ³¼Ä´æÆ÷1 Æ«ÒÆ£º0x9c */
+    __IO uint32_t frm_numl;      /**< \brief ¿ò¼ÜÊı×Ö¼Ä´æÆ÷ Æ«ÒÆ£º0xa0 */
+    __IO uint32_t frm_numh;      /**< \brief ¿ò¼ÜÊı×Ö¼Ä´æÆ÷ Æ«ÒÆ£º0xa4 */
+    __IO uint32_t token;         /**< \brief Ö¸Áî¼Ä´æÆ÷ Æ«ÒÆ£º0xa8 */
+    __IO uint32_t sof_thld;      /**< \brief SOFãĞ´æÆ÷ Æ«ÒÆ£º0xac */
+    __IO uint32_t bdt_page02;    /**< \brief bufferÃèÊöÒ³¼Ä´æÆ÷2 Æ«ÒÆ£º0xb0 */
+    __IO uint32_t bdt_page03;    /**< \brief bufferÃèÊöÒ³¼Ä´æÆ÷3 Æ«ÒÆ£º0xb4 */
+    __I  uint32_t reserve17[2];  /**< \brief ±£Áô  */
+    __IO uint32_t endpt[16];     /**< \brief ¶Ëµã¿ØÖÆ¼Ä´æÆ÷ Æ«ÒÆ£º0xc0 ~ 0xfc */
 
 }amhw_zmf159_usb_t;
+
+typedef struct amhw_zmf159_bdt_data {
+    __IO uint32_t format;
+    __IO uint32_t address;
+} amhw_zmf159_bdt_data_t;
+
+typedef struct amhw_zmf159_usb_bdt {
+    amhw_zmf159_bdt_data_t rx_buf[2];
+    amhw_zmf159_bdt_data_t tx_buf[2];
+} amhw_zmf159_usb_bdt_t;
 
 /**
  * \beief ÖĞ¶ÏÊ¹ÄÜ
@@ -109,10 +86,52 @@ typedef enum amhw_zmf159_int_enb{
     ZMF159_STALL,
 } amhw_zmf159_int_enb_t;
 
-//typedef enum amhw_zmf159_dev_stat{
-//    ZMF159_MASTER_DEV      = 0,
-//    ZMF159_SLAVE_DEV       = 1
-//} amhw_zmf159_dev_stat_t;
+/**
+ * \beief CTRL¼Ä´æÆ÷ÉèÖÃ
+ */
+typedef enum amhw_zmf159_ctrl{
+    ZMF159_USB_EN   = 0,
+    ZMF159_ODD_RST,
+    ZMF159_RESUME,
+    ZMF159_TOK_DNE,
+    ZMF159_SLEEP,
+    ZMF159_RESUME,
+    ZMF159_ATTACH,
+    ZMF159_STALL,
+} amhw_zmf159_ctrl_t;
+
+/**
+ * \brief ÅäÖÃÃèÊö·ûµØÖ·
+ */
+am_static_inline
+void amhw_zmf159_bdt_page_set (amhw_zmf159_usb_t *p_hw_usb,
+                               uint32_t           addr)
+{
+    p_hw_usb->bdt_page03 = (addr >> 24) & 0xff;
+    p_hw_usb->bdt_page02 = (addr >> 16) & 0xff;
+    p_hw_usb->bdt_page01 = ((addr >> 9) & 0xff) << 1;
+}
+
+/**
+ * \brief ÉèÖÃUSBµØÖ·
+ */
+am_static_inline
+void amhw_zmf159_addr_set (amhw_zmf159_usb_t *p_hw_usb,
+                           uint8_t            usb_addr)
+{
+    p_hw_usb->addr = (p_hw_usb->addr & ~0xef) | (usb_addr & 0xef);
+}
+
+/**
+ * \brief USB ctrlÉèÖÃ
+ */
+am_static_inline
+void amhw_zmf159_ctrl_set (amhw_zmf159_usb_t *p_hw_usb,
+                           amhw_zmf159_ctrl_t ctrl,
+                           am_bool_t          flag)
+{
+    (flag) ? (p_hw_usb->ctl |= (1 << ctrl)) : (p_hw_usb->ctl &= ~(1 << ctrl));
+}
 
 /**
  * \brief ÉèÖÃÖĞ¶ÏÊ¹ÄÜ
@@ -153,76 +172,14 @@ uint8_t amhw_zmf159_int_num_get (amhw_zmf159_usb_t *p_hw_usb)
 }
 
 /**
- * \brief USB Ê¹ÄÜ
+ * \brief USB »ñÈ¡µ±Ç°BDT¸üĞÂµÄ¶Ëµã
  */
 am_static_inline
-void amhw_zmf159_usb_enable (amhw_zmf159_usb_t *p_hw_usb)
+uint8_t amhw_zmf159_usb_bdt_ep_get (amhw_zmf159_usb_t *p_hw_usb)
 {
-    p_hw_usb->ctl |= (1 << 0);
+    return (uint8_t)((p_hw_usb->stat >> 4) & 0x0f);
 }
 
-/**
- * \brief USB ½ûÄÜ
- */
-am_static_inline
-void amhw_zmf159_usb_disable (amhw_zmf159_usb_t *p_hw_usb)
-{
-    p_hw_usb->ctl &= ~(1 << 0);
-}
-
-/**
- * \brief ODD ÖØÖÃ
- */
-am_static_inline
-void amhw_zmf159_odd_rest (amhw_zmf159_usb_t *p_hw_usb)
-{
-    p_hw_usb->ctl |= (1 << 1);
-}
-
-/**
- * \brief USB-FS »Ö¸´ĞÅºÅ
- */
-am_static_inline
-void amhw_zmf159_usb_resume (amhw_zmf159_usb_t *p_hw_usb)
-{
-    p_hw_usb->ctl |= (1 << 2);
-}
-
-/**
- * \brief ÉèÖÃUSBÎªÖ÷»úÄ£Ê½
- */
-am_static_inline
-void amhw_zmf159_host_mode_enable (amhw_zmf159_usb_t *p_hw_usb)
-{
-    p_hw_usb->ctl |= (1 << 3);
-}
-
-/**
- * \brief USB ²úÉú ¸´Î»ĞÅºÅ£¨½öÖ÷»úÄ£Ê½ÏÂ£©
- */
-am_static_inline
-void amhw_zmf159_usb_reset_start (amhw_zmf159_usb_t *p_hw_usb)
-{
-    p_hw_usb->ctl |= (1 << 4);
-}
-
-/**
- * \brief USB ½áÊø¸´Î»ĞÅºÅ£¨½öÖ÷»úÄ£Ê½ÏÂ£©
- */
-am_static_inline
-void amhw_zmf159_usb_reset_end (amhw_zmf159_usb_t *p_hw_usb)
-{
-    p_hw_usb->ctl &= ~(1 << 4);
-}
-
-/**
- * \brief USB »ñÈ¡×´Ì¬¼Ä´æÆ÷
- */
-am_static_inline
-uint8_t amhw_zmf159_usb_stat_get (amhw_zmf159_usb_t *p_hw_usb)
-{
-    return (uint8_t)(p_hw_usb->stat);
-}
 
 /**
  * @}if_amhw_zmf159_usb
