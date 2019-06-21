5cf80fd1efdee06e310a1dd509174e71346e7fca
yizhong <yizhong@zlg.cn>
2019-06-21 16:21:08 +0800

    修改ZSN60x例程相关注释以及说明

--- examples/components/drivers/demo_zsn60x_led_test.c
+++ examples/components/drivers/demo_zsn60x_led_test.c
@@ -1,3 +1,40 @@
+/*******************************************************************************
+*                                 AMetal
+*                       ----------------------------
+*                       innovating embedded platform
+*
+* Copyright (c) 2001-2018 Guangzhou ZHIYUAN Electronics Co., Ltd.
+* All rights reserved.
+*
+* Contact information:
+* web site:    http://www.zlg.cn/
+*******************************************************************************/
+
+/**
+ * \file
+ * \brief zsn60x LED测试程序
+ *
+ * - 操作步骤：
+ *   1. 正确连接并配置好串口；
+ *
+ * - 实验现象：
+ *   1. ZSN600的LED灯将会闪烁，若执行错误则会打印响应的错误标识号 ；
+ *
+ * \par 源代码
+ * \snippet demo_zsn60x_led_test.c src_zsn60x_led_test
+ *
+ * \internal
+ * \par Modification history
+ * - 1.00 19-06-18  htf, first implementation.
+ * \endinternal
+ */
+
+/**
+ * \addtogroup demo_if_zsn60x_led_test
+ * \copydoc demo_zsn60x_led_test.c
+ */
+
+/** [src_zsn60x_led_test] */
 #include "zsn60x.h"
 #include "ametal.h"
 #include "am_vdebug.h"
@@ -18,3 +55,6 @@ void demo_zsn60x_led_test_entry (zsn60x_handle_t handle)
         am_kprintf("led control beacuse error 0x%02x", ret);
     }
 }
+/** [src_zsn60x_led_test] */
+
+/* end of file */
