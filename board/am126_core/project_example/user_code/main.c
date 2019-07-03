7ed694672a4a9a89207914958c812f26d58728ac
unknown <fanzhibin@zlgmcu.com>
2019-07-03 14:52:01 +0800

    修改main.c

--- board/am126_core/project_example/user_code/main.c
+++ board/am126_core/project_example/user_code/main.c
@@ -43,7 +43,7 @@ int am_main (void)
      *
      * 注意：同一时刻只能运行一个 demo，即只能使某一行处于取消注释状态。
      */
-//    demo_am116_core_std_led_entry();
+    demo_am116_core_std_led_entry();
 //    demo_am116_core_std_delay_entry();
 //    demo_am116_core_hw_adc_int_entry();
 //    demo_am116_core_std_adc_entry();
@@ -139,7 +139,7 @@ int am_main (void)
 //    demo_am116_zsn60x_piccb_test_entry();
 //    demo_am116_zsn60x_auto_detect_test_entry();
 //    demo_zlg126_usbd_keyboard_entry();
-    demo_zlg126_usbd_printer_entry();
+//    demo_zlg126_usbd_printer_entry();
 //    demo_zlg126_usbd_msc_entry();
 //    demo_zlg126_usbd_vcom_entry();
     while (1) {
